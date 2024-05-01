20f6bc254bd0d9d786538a612268b7378ec09530
RyanFin <ryanfinlayson125@hotmail.com>
2024-05-01 19:19:25 +0100

    add static version of site

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,15 @@
+import AppNav from "../AppNav/AppNav";
+import Footer from "../Footer/Footer";
+import Logo from "../Logo/Logo";
+import styles from "./Sidebar.module.css";
+export default function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+
+      <p>List of cities</p>
+      <Footer />
+    </div>
+  );
+}
