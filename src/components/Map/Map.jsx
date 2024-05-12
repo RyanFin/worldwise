7ddb4c5e9f9ec52239be388d959e75a19600f89c
RyanFin <ryanfinlayson125@hotmail.com>
2024-05-12 18:16:25 +0100

    lock position of selected country to map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,8 @@
+import PropTypes from "prop-types";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import { useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import { useEffect, useState } from "react";
 import { useCities } from "../../contexts/CitiesContext";
 
 export default function Map() {
@@ -9,32 +10,42 @@ export default function Map() {
   const navigate = useNavigate();
   const { cities } = useCities();
   // eslint-disable-next-line no-unused-vars
-  const [mapPosition, setMapPosition] = useState([40, 0]);
   // eslint-disable-next-line no-unused-vars
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
   // eslint-disable-next-line no-unused-vars
   const mapLat = searchParams.get("lat"); // get value from URL
   // eslint-disable-next-line no-unused-vars
   const mapLng = searchParams.get("lng");
 
+  // const values as these values are NOT changing anytime soon
   const startingLat = 51.500833;
   const startingLng = -0.141944;
 
-  console.log([mapLat, mapLng]);
-  console.log(mapPosition);
+  const [mapPosition, setMapPosition] = useState([startingLat, startingLng]);
+  // const mapPos = [
+  //   // if null set a default starter city
+  //   mapLat === null ? startingLat : mapLat,
+  //   mapLng === null ? startingLng : mapLng,
+  // ];
+
+  // useEffect is a synchronisation method
+  useEffect(
+    function () {
+      if (mapLat && mapLng) {
+        setMapPosition([mapLat, mapLng]);
+      }
+    },
+    [mapLat, mapLng]
+  );
 
   return (
     // navigate to 'form' url on click
     // <div className={styles.mapContainer} onClick={() => navigate("form")}>
     <div className={styles.mapContainer}>
       <MapContainer
-        center={[
-          // if null set a default starter city
-          mapLat === null ? startingLat : mapLat,
-          mapLng === null ? startingLng : mapLng,
-        ]}
+        // center={mapPos}
         // center={[mapLat, mapLng]}
-        // center={mapPosition}
+        center={mapPosition}
         zoom={6}
         scrollWheelZoom={true}
         className={styles.map}
@@ -54,7 +65,24 @@ export default function Map() {
             </Popup>
           </Marker>
         ))}
+        <ChangeCenter
+          // Default values set
+          // position={[mapLat || startingLat, mapLng || startingLng]}
+          position={mapPosition}
+        />
       </MapContainer>
     </div>
   );
+}
+
+// separate component
+function ChangeCenter({ position }) {
+  ChangeCenter.propTypes = {
+    position: PropTypes.array,
+  };
+  // get the current instance of the map being displayed
+  const map = useMap();
+  map.setView(position);
+
+  return null;
 }
