b8a78b43afed7f0586fb26358372b173f2c7e5ec
RyanFin <ryanfinlayson125@hotmail.com>
2024-05-18 13:30:57 +0100

    make server changes

--- server.mjs
+++ server.mjs
@@ -1,7 +1,11 @@
-const express = require("express");
-const path = require("path");
-const { createProxyMiddleware } = require("http-proxy-middleware");
-const jsonServer = require("json-server");
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import { createProxyMiddleware } from "http-proxy-middleware";
+import jsonServer from "json-server";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
