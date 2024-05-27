2fd561c7deff339aa11901834c2ce3674b756bf5
RyanFin <ryanfinlayson125@hotmail.com>
2024-05-27 17:21:34 +0100

    use server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("data/cities.json");
+const middlewares = jsonServer.defaults({ static: "./build" });
+// on Heroku port if defined or port 9000 by default
+const port = process.env.PORT || 9000;
+
+// assign values to the server
+server.use(middlewares);
+server.use(router);
+
+// run json-server at target port
+server.listen(port);
