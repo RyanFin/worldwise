bdbaf02b00cac78a1b8cc3763a0d544bc9fb35a9
RyanFin <ryanfinlayson125@hotmail.com>
2024-05-27 19:19:00 +0100

    try vanilla json-server

---
+++ db.js
@@ -0,0 +1,5 @@
+const cities = require("./data/cities");
+
+module.exports = {
+  cities,
+};
--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-import jsonServer from "json-server";
+const data = require("./db.js");
+
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("data/cities.json");
-const middlewares = jsonServer.defaults({ static: "./build" });
-// on Heroku port if defined or port 9000 by default
+const router = jsonServer.router(data);
+const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 9000;
 
-// assign values to the server
 server.use(middlewares);
 server.use(router);
 
-// run json-server at target port
 server.listen(port);
